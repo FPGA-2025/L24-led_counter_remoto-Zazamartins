39e952b17345264a5973826fe819a8b3ec47b29b
Zazamartins <isaacmrtnsss@gmail.com>
2025-07-07 15:02:53 -0300

    Talvez

--- led_counter.v
+++ led_counter.v
@@ -6,5 +6,25 @@ module Counter #(
     output wire [7:0] leds
 );
 
+    reg [31:0] cycle_count;
+    reg [7:0] count_reg;
+
+    localparam integer CYCLES = CLK_FREQ / 2;
+
+    always @(posedge clk or negedge rst_n) begin
+        if (!rst_n) begin
+            cycle_count <= 32'd0;
+            count_reg   <= 8'd0;
+        end else begin
+            if (cycle_count == CYCLES - 1) begin
+                cycle_count <= 32'd0;
+                count_reg   <= count_reg + 8'd1;
+            end else begin
+                cycle_count <= cycle_count + 32'd1;
+            end
+        end
+    end
+
+    assign leds = count_reg;
 
 endmodule
